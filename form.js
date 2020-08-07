700d9c8fa0193b99fdf39264c303f5e74a1f07cb
Chethankrishna2005 <67218224+Chethankrishna2005@users.noreply.github.com>
2020-08-07 14:15:42 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,32 @@
+class Form{
+
+constructor () {
+
+
+
+}
+
+display() {
+
+    //h2 means heading sizes
+    var title=createElement('h2');
+    title.html("car racing game");
+    title.position(130,0);
+    var input = createInput("Name");
+    var button = createButton('play');
+    var greeting = createElement('h3');
+    input.position(130,160);
+    button.position(250,200);
+    button.mousePressed(function() {
+
+        input.hide();
+        button.hide();
+        var name = input.value();
+        greeting.html("hello" + name);
+        greeting.position(130,160);
+
+    });
+
+}
+
+}
